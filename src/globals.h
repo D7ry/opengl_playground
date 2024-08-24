5f9ae45bdf52e18eaa7773057bb79776b00e312d
d7ryLinux <yty108102@gmail.com>
2024-08-24 15:57:41 -0700

    clean up includes

---
+++ src/globals.h
@@ -0,0 +1,14 @@
+// stl
+#include <string>
+#include <vector>
+
+#include <glm/glm.hpp>
+
+// opengl
+// clang-format off
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// clang-format on
+
+const glm::vec3 WORLD_UP_VECTOR = {0.f, 1.f, 0.f};
